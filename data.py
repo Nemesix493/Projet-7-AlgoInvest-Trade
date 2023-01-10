2592c66f11fb154aa98c4bc31033e4b00c407b84
Nemesix493 <verquerre.kevin@gmail.com>
2023-01-10 12:35:49 +0100

    loading the data

---
+++ data.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+actions = {
+    'name': [],
+    'price': [],
+    'moneyReturnRate': []
+}
+actions = pd.DataFrame(actions)
+
+
+def add_actions_to_actions_list(actions_list: pd.DataFrame, name: str, price: int,
+                                money_return_rate: int) -> pd.DataFrame:
+    return pd.concat(
+        [
+            actions_list,
+            pd.DataFrame({
+                'name': [name],
+                'price': [price],
+                'moneyReturnRate': [money_return_rate]
+            })
+        ],
+        ignore_index=True
+    )
+
+
+with open('actions.txt', 'r') as data:
+    for line in data:
+        current_action = line.replace('%', '').replace(' ', '').replace('\n', '').split('\t')
+        actions = add_actions_to_actions_list(
+            actions_list=actions,
+            name=current_action[0],
+            price=int(current_action[1]),
+            money_return_rate=int(current_action[2])
+        )
+print(actions)
