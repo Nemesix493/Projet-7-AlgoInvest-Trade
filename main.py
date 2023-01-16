202ee614ae4a8fc38a4bb6b72889155aac302f06
Nemesix493 <verquerre.kevin@gmail.com>
2023-01-17 00:48:39 +0100

    update main file and .gitignore

--- main.py
+++ main.py
@@ -1,50 +1,34 @@
-import time
+from controllers import main_menu
+from views import TerminalViews
 
-import pandas as pd
 
-import calc
-import data
-import bruteforce
-import optimised
+def main():
+    view = TerminalViews()
+    main_menu(view=view)
 
-step = 50
-step_nbr = 20
 
-data_report = pd.DataFrame({'Brute time / Opti time': []})
-for i in range(step_nbr):
-    print(f'\nOptimised calculating for {(i + 1) * step} €')
-    opti_time = time.time()
-    opti_actions_list = optimised.get_best_invest_action_list(data.actions, (i + 1) * step).sort_values(by=['price'])
-    opti_time = time.time() - opti_time
-    print(f'Bruteforce calculating for {(i + 1) * step} €')
-    brute_time = time.time()
-    brute_actions_list = bruteforce.get_best_invest_action_list(data.actions, (i + 1) * step).sort_values(by=['price'])
-    brute_time = time.time() - brute_time
-    data_report = pd.concat(
-        [
-            data_report,
-            pd.DataFrame({
-                'Investing': [(i + 1) * step],
-                'Bruteforce time': [brute_time],
-                'Bruteforce balance': [calc.action_list_balance(brute_actions_list)],
-                'Optimised time': [opti_time],
-                'Optimised balance': [calc.action_list_balance(opti_actions_list)],
-                'Brute time / Opti time': [brute_time/opti_time],
-                'Bruteforce balance - Optimised balance': [
-                    calc.action_list_balance(opti_actions_list) - calc.action_list_balance(brute_actions_list)
-                ],
-                '(Brute time / Opti time) Mean': [
-                    pd.concat(
-                        [
-                            data_report['Brute time / Opti time'].to_frame(),
-                            pd.DataFrame({
-                                'Brute time / Opti time': [brute_time/opti_time]
-                            })
-                        ]
-                    )['Brute time / Opti time'].mean()
-                ]
-            })
-        ],
-        ignore_index=True
-    )
-    data_report.to_csv('report.csv')
+if __name__ == '__main__':
+    main()
+
+"""import pandas as pd
+
+from controllers.inoutdata import data
+from controllers.dataprocessing import optimised, bruteforce
+from controllers.testing import backtest
+
+tt = backtest.backtest(
+    data.actions,
+    {
+        'Brute': bruteforce.get_best_invest_action_list,
+        'Opti 1': optimised.get_best_invest_action_list,
+        'Opti 2': optimised.test_opti_two
+    },
+    max_invest_price=50
+)
+
+data_data = {'Budget': 50}
+for key, val in tt[0].items():
+    for sub_key, sub_val in val.items():
+        if sub_key != 'Budget':
+            data_data[f'{key}-{sub_key}'] = [sub_val]
+pd.DataFrame(data_data).to_csv('data_data.csv')"""
